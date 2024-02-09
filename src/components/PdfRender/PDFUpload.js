9c7836c1da616d9c67dc2d8e0e411036c8a0cd3b
Arpan De <arpandesp7890@gmail.com>
2024-02-09 07:03:05 +0530

    mini

--- src/components/PdfRender/PDFUpload.js
+++ src/components/PdfRender/PDFUpload.js
@@ -1,35 +1,42 @@
 // PDFUpload.js
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
-import PdfViewer from './PdfViewer';
-
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import PdfViewer from "./PdfViewer";
 
 const PDFUpload = () => {
-    const [pdf, setPdf] = useState(null);
+  const [pdf, setPdf] = useState(null);
 
-    const handleFileUpload = (event) => {
-      const file = event.target.files[0];
-      if (file) {
-        setPdf(file);
-      }
-    };
-    const close=()=>{
-        setPdf(null);
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setPdf(file);
     }
-  
-    return (
-      <div className="App flex flex-col items-center justify-center min-h-screen py-6 bg-gray-100">
+  };
+  const close = () => {
+    setPdf(null);
+  };
+
+  return (
+    <div className="App flex flex-col items-center justify-center min-h-screen py-6 bg-gray-100">
       <h1 className="text-3xl font-semibold mb-8">PDF Viewer</h1>
       <div className="flex items-center space-x-4 mb-4">
-        <input type="file" accept=".pdf,.docx" onChange={handleFileUpload} className="py-2 px-4 border border-gray-300 rounded-lg bg-white text-gray-800 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
-        <button onClick={close} className="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">Close</button>
+        <input
+          type="file"
+          accept=".pdf,.docx"
+          onChange={handleFileUpload}
+          className="py-2 px-4 border border-gray-300 rounded-lg bg-white text-gray-800 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+        />
+        <button
+          onClick={close}
+          className="py-2 px-4 bg-red-500 text-white font-semibold rounded-lg shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+        >
+          Close
+        </button>
       </div>
       {pdf && <PdfViewer pdf={pdf} />}
     </div>
-    );
+  );
 };
 
 export default PDFUpload;
-
-
--- src/components/PdfRender/PdfViewer.js
+++ src/components/PdfRender/PdfViewer.js
@@ -1,39 +1,45 @@
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf";
 
 const PdfViewer = ({ pdf }) => {
-    const [numPages, setNumPages] = useState(null);
+  const [numPages, setNumPages] = useState(null);
 
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-    }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
-    const renderPages = () => {
-        const pages = [];
-        for (let i = 1; i <= numPages; i++) {
-            pages.push(
-                <div key={i} className="pdf-page">
-                    <Page
-                        pageNumber={i}
-                        renderTextLayer={false}
-                        renderAnnotationLayer={false}
-                    />
-                    <p className="flex justify-center">{i} of {numPages}</p>
-                </div>
-            );
-        }
-        return pages;
-    };
-
-    return (
-        <div className='flex justify-center'>
-             <div className="pdf-div" style={{ width: '1070px', height: '600px', overflowY: 'auto' }}>
-                <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
-                    {numPages && renderPages()}
-                </Document>
-            </div>
+  const renderPages = () => {
+    const pages = [];
+    for (let i = 1; i <= numPages; i++) {
+      pages.push(
+        <div key={i} className="pdf-page">
+          <Page
+            scale={1.0}
+            pageNumber={i}
+            renderTextLayer={false}
+            renderAnnotationLayer={false}
+          />
+          <p className="flex justify-center">
+            {i} of {numPages}
+          </p>
         </div>
-    );
+      );
+    }
+    return pages;
+  };
+
+  return (
+    <div className="flex ">
+      <div
+        className="pdf-div flex justify-center"
+        style={{ width: "1080px", height: "600px", overflowY: "auto" }}
+      >
+        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+          {numPages && renderPages()}
+        </Document>
+      </div>
+    </div>
+  );
 };
 
 export default PdfViewer;
